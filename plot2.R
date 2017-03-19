8b5d619467b830e66e492902e5fdcc66c3e8c8f4
Connor Lenio <cojamalo@gmail.com>
2017-03-19 15:03:04 -0400

    Initial commit of all local project files

---
+++ plot2.R
@@ -0,0 +1,26 @@
+library(data.table)
+library(dplyr)
+library(lubridate)
+
+### Working Directory
+##Please make sure your working directory contains the uncompressed contents from https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+##The script assumes that paths such as "./household_power_consumption.txt" are valid. Please confirm this is the case for you.
+
+###Data Import and Cleaning Steps
+##Import the data using fread, replacing '?' with NAs and keeping the headers, convert to tibble dataframe, and filter to desired dates
+df <- fread("./household_power_consumption.txt", header= TRUE, na.strings = "?") %>%
+  tbl_df %>%
+  filter(Date == "1/2/2007" | Date == "2/2/2007")
+
+##Combine the date and time variables into a single Datetime character vector and convert to date-time format.
+df$Datetime <- paste(df$Date, df$Time, sep = " ") %>%
+                dmy_hms
+##Reorder dataframe variables to exclude old Date and Time character vectors
+df <- select(df, Datetime, Global_active_power:Sub_metering_3)
+
+###Construct the Plot
+png("plot2.png", width = 480, height = 480)
+
+with(df, plot(x= Datetime, y = Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)"))
+
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,44 @@
+library(data.table)
+library(dplyr)
+library(lubridate)
+
+### Working Directory
+##Please make sure your working directory contains the uncompressed contents from https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+##The script assumes that paths such as "./household_power_consumption.txt" are valid. Please confirm this is the case for you.
+
+###Data Import and Cleaning Steps
+##Import the data using fread, replacing '?' with NAs and keeping the headers, convert to tibble dataframe, and filter to desired dates
+df <- fread("./household_power_consumption.txt", header= TRUE, na.strings = "?") %>%
+  tbl_df %>%
+  filter(Date == "1/2/2007" | Date == "2/2/2007")
+
+##Combine the date and time variables into a single Datetime character vector and convert to date-time format.
+df$Datetime <- paste(df$Date, df$Time, sep = " ") %>%
+  dmy_hms
+
+##Reorder dataframe variables to exclude old Date and Time character vectors
+df <- select(df, Datetime, Global_active_power:Sub_metering_3)
+
+###Construct the Plot
+png("plot4.png", width = 480, height = 480)
+
+##Adjust base plotting parameters for 2x2 layout
+par(mfrow=c(2,2))
+
+##Make plot 1
+with(df, plot(x= Datetime, y = Global_active_power, type = "l", xlab = "", ylab = "Global Active Power"))
+
+##Make plot 2
+with(df, plot(x= Datetime, y = Voltage, type = "l", xlab = "datetime", ylab = "Voltage"))
+
+##Make plot 3
+with(df, plot(x= Datetime, y = Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering"))
+with(df, lines(x= Datetime, y = Sub_metering_2, col = "red"))
+with(df, lines(x= Datetime, y = Sub_metering_3, col = "blue"))
+legend("topright", legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col = c("black", "red", "blue"), lty=c(1,1,1))
+
+##Make plot 4
+with(df, plot(x= Datetime, y = Global_reactive_power, type = "l", xlab = "datetime"))
+
+dev.off()
+
